3d0ed3af65cab7bb25d30ff6fc4e6358eb9e54bd
janeamy <jane.ammie@gmail.com>
2022-10-10 17:09:58 -0700

    commit

--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,3 +1,10 @@
-#ifndef SIZE
-#define SIZE 1024
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
+
 #endif
