328330a01baaa8efe045a70361866786c22a50ce
janeamy <jane.ammie@gmail.com>
2022-10-28 05:50:38 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-* binary_to_unit - converts a binary number to an
-* unsigned int.
-* @b: binary
-*
-* Return: unsigned int
-*/
-unsigned int binary_to_unit(const char *b)
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
 	int len, base_two;
@@ -18,6 +18,7 @@ unsigned int binary_to_unit(const char *b)
 	ui = 0;
 
 	for (len = 0; b[len] != '\0'; len++)
+		;
 
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
